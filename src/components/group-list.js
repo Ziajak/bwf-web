3153c77d55ec13909c7421aec94efd5dde46085b
Ziajak <radoslawmichalek86@gmail.com>
2025-07-31 14:29:32 +0200

    Adding group-list.js and install Material UI

---
+++ src/components/group-list.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react'
+
+
+function GroupList() {
+
+    const [groups, setGroups] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(()=>{
+
+        setLoading(true);
+        const getData = async() => {
+            await fetch('http://127.0.0.1:8000/api/groups/').then(resp => resp.json())
+                .then( data => {
+                    setGroups(data);
+                    setLoading(false);
+                }).catch(e =>{
+
+                    setError(true);
+                    setLoading(false);
+                })
+
+        }
+        getData();
+    }, [])
+
+    if (error) return  <h1>Error</h1>
+    if (loading) return <h1>Loading...</h1>
+
+
+    return (
+    <div>
+        { groups && groups.map(group => {
+          return <p>{group.name}: {group.location}</p>
+          })
+      }
+    </div>
+  );
+}
+
+export default GroupList;
