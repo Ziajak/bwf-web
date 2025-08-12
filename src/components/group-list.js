07dd9f9cd1874ff14445907a1df2a9a91b8727b2
Ziajak <radoslawmichalek86@gmail.com>
2025-08-12 16:36:44 +0200

    Links for the details

--- src/components/group-list.js
+++ src/components/group-list.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-
+import { Link } from 'react-router-dom';
 
 function GroupList() {
 
@@ -32,8 +32,10 @@ function GroupList() {
     return (
     <div>
         { groups && groups.map(group => {
-          return <p key={group.id}>{group.name}: {group.location}</p>
-          })
+          return <Link key={group.id} to={`/details/${group.id}`}>
+                <p>{group.name}: {group.location}</p>
+            </Link>
+            })
       }
     </div>
   );
