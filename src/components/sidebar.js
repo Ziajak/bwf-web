da5a6c89e59606b0a37149d6e355883531b0f6ad
Ziajak <radoslawmichalek86@gmail.com>
2025-08-21 17:11:02 +0200

    Login UI

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Button } from '@mui/material'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import KeyIcon from '@mui/icons-material/Key';
 function Sidebar() {
 
+    const  [ username, setUsername ] = useState('');
+    const  [ password, setPassword ] = useState('');
+
+    const  handleSubmit = async e => {
+        e.preventDefault();
+        console.log(username, password);
+
+    }
 
     return (
     <div className="sidebar">
-        <h2>Sidebar</h2>
-        <Button variant="contained" color="primary" >
-         My button
-        </Button>
+        <form onSubmit={handleSubmit}>
+         <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <AccountCircle sx={{ mr: 1, my: 0.5 }} />
+            <TextField id="input-with-sx" label="Username" variant="standard"
+                onChange={ e => setUsername(e.target.value)}
+            />
+         </Box>
+         <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <KeyIcon sx={{ mr: 1, my: 0.5 }} />
+            <TextField id="input-with-sx" label="Password" variant="standard" type="password"
+                onChange={ e => setPassword(e.target.value)}
+            />
+         </Box>
 
+
+         <Button variant="contained" color="primary" type="submit">
+          Login
+         </Button>
+        </form>
 
     </div>
   );
