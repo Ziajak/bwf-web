a7a9361bf33d5b1cd7a7a88aa563171e0bd4b42d
Ziajak <radoslawmichalek86@gmail.com>
2025-12-22 16:07:48 +0100

    Fetch event details

--- src/components/events/event.js
+++ src/components/events/event.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {DateTime} from "luxon";
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import { styled } from "@mui/material/styles";
+import { useParams} from "react-router-dom";
+import {useFetchEvent} from "../../hooks/fetch-event";
+import {useAuth} from "../../hooks/useAuth";
 
 // Stylowane ikony
 const StyledCalendarIcon = styled(CalendarTodayIcon)(({ theme }) => ({
@@ -28,10 +31,26 @@ const StyledAlarmIcon = styled(AccessAlarmIcon)(({ theme }) => ({
 
 export function Event(){
 
+    const {authData} = useAuth();
+    const { id } = useParams();
+    const [data, loading, error] = useFetchEvent(authData.token, id);
+    const [event, setEvent] = useState(null);
 
+        useEffect(()=>{
+        if(data){
+             setEvent(data);
+        }
+    }, [data])
     return (
         <React.Fragment>
                 <h3>Event info here:</h3>
+            {event && (
+                <>
+                    {event.team1} vs. {event.team2}
+                </>
+            )}
+
+
 
 
 
